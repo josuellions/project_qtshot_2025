141f26ca8b68939855189837d2b580060c098988
josuellions <josuellions@gmail.com>
2025-07-15 02:21:12 -0300

    refactor: restructure `participants` list layout

--- src/app/participants/page.tsx
+++ src/app/participants/page.tsx
@@ -32,75 +32,77 @@ export default function Participants() {
     <>
       <Header className="items-start bg-stone-200 border-2 border-stone-400" />
 
-      <Body className="flex flex-col justify-start relative pb-8">
-        <div className="flex flex-col gap-8 p-10 overflow-y-scroll">
+      <Body className="flex flex-col justify-start relative pb-8 overflow-hidden">
+        <div className="flex flex-col gap-8 w-full mt-4">
           <h1 className="text-center text-2xl capitalize font-semibold text-stone-700">
             Participantes
           </h1>
 
           {!data.events && (
-            <div className="flex flex-col h-screen items-center justify-center">
+            <div className="flex flex-col max-h-[62vh] h-[62vh] items-center justify-center">
               <Loader className="animate-spin w-32 h-32 text-stone-400" />
             </div>
           )}
 
-          {data.events &&
-            Object.entries(data.events).map(([date, participants]: any) => (
-              <div key={date}>
-                <h3 className="font-medium pb-1 flex flex-row items-center">
-                  <span className="capitalize flex flex-row item-center h-8 min-h-8 gap-2 mb-2">
-                    <span className="flex flex-row  ml-2 w-8 h-8 min-w-8 min-h-8 justify-center items-center text-xl bg-stone-500 text-stone-100 p-1 rounded-full">
-                      {participants.total_participants}
-                    </span>
-                    <span className="flex flex-col">
-                      {/* {new Date(participants.date).toLocaleDateString("pt-BR")} */}
-                      <span className="font-semibold text-sm">
-                        {formatDateDayOfTheWeek(date)}
+          <div className="overflow-y-auto flex flex-col max-h-[62vh] pb-10 w-full gap-8">
+            {data.events &&
+              Object.entries(data.events).map(([date, participants]: any) => (
+                <div key={date}>
+                  <div className="font-medium pb-1 flex flex-row items-center">
+                    <span className="capitalize flex flex-row item-center h-8 min-h-8 gap-2 mb-2">
+                      <span className="flex flex-row p-1 mt-1 ml-2 w-6 h-6 min-w-6 min-h-6 justify-center items-center text-xs bg-stone-300 text-stone-700 rounded-full">
+                        <small>{participants.total_participants}</small>
                       </span>
-                      <span className="text-xs capitalize font-light -mt-1">
-                        <small>{formatDateExtensive(date)}</small>
+                      <span className="flex flex-col">
+                        {/* {new Date(participants.date).toLocaleDateString("pt-BR")} */}
+                        <span className="font-semibold text-sm">
+                          {formatDateDayOfTheWeek(date)}
+                        </span>
+                        <span className="text-xs capitalize font-light -mt-1">
+                          <small>{formatDateExtensive(date)}</small>
+                        </span>
                       </span>
                     </span>
-                  </span>
-                </h3>
+                  </div>
 
-                {/* <pre className="text-xs p-10 ml-24">
+                  {/* <pre className="text-xs p-10 ml-24">
                     {JSON.stringify(participants.participants, null, 2)}
                   </pre> */}
 
-                <ul className="flex flex-col gap-3">
-                  {participants?.participants.map((participant: any) => (
-                    <li
-                      key={participant.date_at}
-                      className="flex items-center gap-2 bg-gray-200 p-2 rounded-md cursor-pointer"
-                    >
-                      <span className="flex flex-row gap-2 text-sm text-zinc-400 items-center">
-                        <span className="relative flex flex-row -mt-1 w-8 h-8 min-w-8 min-h-8 justify-center items-center text-xl bg-stone-500 text-stone-100 p-1 rounded-full">
-                          <Image
-                            fill
-                            unoptimized
-                            alt="Foto participante"
-                            src={participant.image_base64}
-                            className="w-8 h-8 max-w-8 max-h-8 rounded-full overflow-hidden absolute"
-                          />
-                        </span>{" "}
-                        <span className="flex flex-row items-center -mt-2">
-                          <Link
-                            target="_blank"
-                            href={participant.image_url}
-                            className="text-2xl font-bold hover:underline hover:decoration-1 hover:text-underline-offset-4 underline decoration-2 text-underline-offset-4"
-                          >
-                            <small className="text-xs text-gray-700 ">
-                              click para acesse a foto do evento{" "}
-                            </small>
-                          </Link>
+                  <ul className="flex flex-col gap-3 px-10">
+                    {participants?.participants.map((participant: any) => (
+                      <li
+                        key={participant.date_at}
+                        className="flex items-center gap-2 bg-stone-200 hover:bg-stone-300 text-stone-700 hover:text-stone-400 p-2 rounded-md cursor-pointer"
+                      >
+                        <span className="flex flex-row gap-2 text-sm items-center">
+                          <span className="relative flex flex-row -mt-1 w-8 h-8 min-w-8 min-h-8 justify-center items-center text-xl p-1 rounded-full">
+                            <Image
+                              fill
+                              unoptimized
+                              alt="Foto participante"
+                              src={participant.image_base64}
+                              className="w-8 h-8 max-w-8 max-h-8 rounded-full overflow-hidden absolute"
+                            />
+                          </span>{" "}
+                          <span className="flex flex-row items-center -mt-2">
+                            <Link
+                              target="_blank"
+                              href={participant.image_url}
+                              className="text-2xl font-bold underline decoration-1 decorator-stone-700 underline-offset-4  hover:underline hover:decoration-2 hover:text-underline-offset-4 hover:decorator-stone-100"
+                            >
+                              <small className="text-xs  ">
+                                click para acesse a foto do evento{" "}
+                              </small>
+                            </Link>
+                          </span>
                         </span>
-                      </span>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+          </div>
         </div>
       </Body>
       <div className="flex w-full flex-row gap-4">
